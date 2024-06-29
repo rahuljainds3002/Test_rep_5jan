164e83688bce20970f9f8bbf774648726e37b336
rahul jain <rahul.jainds3002@gmail.com>
2024-06-29 19:08:20 +0530

    Assignment1_question_3_scala

--- src/main/scala/AS2.scala
+++ src/main/scala/AS2.scala
@@ -1,9 +1,23 @@
 object as2 {
   def main(args: Array[String]): Unit = {
 
-    var a = -15
-    if(a<10||a>10){
-      print("value is within defined range")
+    var a = 32
+    var b = a%2
+    var c  = a%3
+    //print(c)
+    if(b!=0&&c==0 ){
+      print("number is odd and divisible by 3")
     }
+    else if(b!=0&&c!=0){
+      print("number is  odd but not divisible by 3")
+    }
+    else if(b==0&&c==0) {
+      print("number is not odd and  divisible by 3")
+    }
+    else if(b==0&&c!=0) {
+      print("number is not odd and  not divisible by 3")
+    }
+    else{print("number is not a valid integer" )}
+
   }
 }
