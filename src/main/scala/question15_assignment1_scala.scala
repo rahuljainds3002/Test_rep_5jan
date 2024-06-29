aa88c3435342d39d1e0db30ce6671d35f2783cca
rahul jain <rahul.jainds3002@gmail.com>
2024-06-29 20:15:05 +0530

    Assignment1_question_15_scala

---
+++ src/main/scala/question15_assignment1_scala.scala
@@ -0,0 +1,12 @@
+object question15_assignment1_scala {
+  def main(args: Array[String]): Unit = {
+    var age  = 188
+    if(age <13){println("person is a child")}
+    else if(age >=13&&age<=19){println("person is a teenager")}
+    else if(age >=20){println("person is a adult")}
+    else{println("number is not proper age")}
+
+  }
+
+
+}
