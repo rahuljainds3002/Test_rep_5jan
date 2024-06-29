a97119eae135b0357abc886cd220de196b3876a5
rahul jain <rahul.jainds3002@gmail.com>
2024-06-29 19:40:56 +0530

    Assignment1_question_6_scala

---
+++ src/main/scala/question5_assignment_1_scala.scala
@@ -0,0 +1,17 @@
+object question6_assignment_1_scala {
+  def main(args: Array[String]): Unit = {
+    var a = 22
+
+    if((a>=1&&a<=10)||(a>=20&&a<=30) ){
+      print("number is in range asked")
+    }
+
+    else{print("number is not in valid range" )}
+
+
+
+
+  }
+
+
+}
